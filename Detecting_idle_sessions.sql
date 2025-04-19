653b5c84df593847078cd03cb2e80f8841402ac3
Marcel Luginbuehl <marcel.luginbuehl@bithawk.ch>
2025-04-19 14:43:34 +0200

    Determine idle sessions with open transactions
    The following script searches for sessions that have open transactions and are idle.
    Sessions for which no request is currently being executed are said to be idle.

---
+++ Detecting_idle_sessions.sql
@@ -0,0 +1,21 @@
+-- Determine idle sessions with open transactions									                                                                                  
+-- Firma: BitHawk AG
+-- Script Name: Detecting_idle_sessions.sql
+-- Description: The following script searches for sessions that have open transactions and are idle.
+-- Sessions for which no request is currently being executed are said to be idle.
+-- Created: 18.04.2025
+-- Last Modified:
+*/
+
+SELECT s.*
+FROM sys.dm_exec_sessions AS s
+WHERE EXISTS (
+        SELECT *
+        FROM sys.dm_tran_session_transactions AS t
+        WHERE t.session_id = s.session_id
+    )
+    AND NOT EXISTS (
+        SELECT *
+        FROM sys.dm_exec_requests AS r
+        WHERE r.session_id = s.session_id
+);
